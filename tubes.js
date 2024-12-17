d47200c9c942bbe1ed45f3a732ba54888d34fc4d
JakPaww <mfdzaky06@gmail.com>
2024-12-17 12:23:55 +0700

    first commit

---
+++ tubes.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Add form submission logic here
+    alert('Form submitted successfully!');
+  });
+
